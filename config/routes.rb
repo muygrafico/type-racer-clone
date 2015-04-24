47c8c8b9904f0c75639735b29279f986e12c7416
German Hernandez <muygrafico@gmail.com>
2015-04-24 16:38:48 -0500

    Devise with extra field name working

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'pages/index'
+  get 'pages/index', to: 'pages#index', as: 'public_index'
+  get 'pages/game'
+  root 'pages#index'
+
+
+  devise_for :users, :controllers => { registrations: 'registrations' }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
