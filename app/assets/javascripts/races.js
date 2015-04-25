5d1e2087be9c59db27490b3b4176f6cad750d52d
German Hernandez <muygrafico@gmail.com>
2015-04-24 23:31:23 -0500

    drying JS 0.002% done

--- app/assets/javascripts/races.js
+++ app/assets/javascripts/races.js
@@ -2,64 +2,73 @@ $( document ).ready(function() {
 
   var clearInput = function(who){ who.val('') }
 
-  var word_to_array = function(textContainer){ return array = $(textContainer).text().split(' ') }
-
+  var word_to_array = function( textContainer ){
+    return array = $(textContainer).text().split(' ')
+  }
 
   var strings = word_to_array('#text-to-input');
-  // console.log(strings_array);
 
-  var Timer = function() {
+  var Race = function(next_words_p) {
     this.time = 0;
-    this.start = function(){
+    this.next_words_p = next_words_p;
+    this.viewHelper(next_words_p);
+  }
+
+  Race.prototype = {
+
+   checkWord : function(array, word){
+    word =  word.replace(/\s/g, '');
+    if ( array  == word ) { return true } else { return false }
+    },
+    viewHelper : function(){
+      viewHelperText =  "<strong>" + strings[counter]+ "</strong>" + " " + strings[counter + 1] + " " + strings[counter + 2] + " " + strings[counter + 3];
+      $( this.next_words_p ).html( viewHelperText );
+    },
+    start : function(){
       var that = this;
       window.setInterval(function(){
         that.time += 1000;
         elapsed = Math.floor(that.time / 1000) / 1;
         console.log(elapsed)
       }, 1000);
-    }
-    this.stop = function(){
+    },
+    stop : function(){
       clearInterval( this );
       elapsed = Math.floor(this.time / 1000) / 1;
       return elapsed;
     }
-  }
-  var timer = new Timer();
+}
 
-  $( "body" ).on( "click", ".start-time", function() {
-    timer.start();
-  });
-  $( "body" ).on( "click", ".stop-time", function() {
-    console.log( "current time at click: " + timer.stop() );
-  });
+userRace = new Race('.next-word');
 
-
-
-  var checkWord = function(array, word){
-    word =  word.replace(/\s/g, '');
-    if ( array  == word ) { return true } else { return false }
-  }
-
-  var counter = 0;
-
-  $("#GameInput").keyup(function(e){
+$( "body" ).on( "click", ".start-time", function() {
+  userRace.start();
+});
+$( "body" ).on( "click", ".stop-time", function() {
+  console.log( "current time at click: " + userRace.stop() );
+});
 
 
-    var val = $( this ).val();
-    console.log( val + " : " + strings[counter] );
-    word_validator = checkWord(strings[counter] , val) ;
-    console.log( word_validator );
-    // console.log("Correct!");
+var counter = 0;
 
-     if( e.which == 32  && word_validator == true ) {
+
+userRace.viewHelper('.next-word');
+
+$("#GameInput").keyup(function(e){
+
+ $('.next-word').html( userRace.viewHelper );
+ var val = $( this ).val();
+
+ word_validator = userRace.checkWord(strings[counter] , val) ;
+
+    userRace.viewHelper();
+    if( e.which == 32  && word_validator == true ) {
       clearInput( $( this ) );
       counter ++;
+
+      userRace.viewHelper();
     }
 
-      // console.log(val);
   });
 
-
-
-
-  });
+});
