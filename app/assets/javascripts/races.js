079f991c2f2cf52640b78a745f76dcf6465f8f25
German Hernandez <muygrafico@gmail.com>
2015-04-24 20:03:56 -0500

    finish string checker

--- app/assets/javascripts/races.js
+++ app/assets/javascripts/races.js
@@ -1,50 +1,55 @@
 $( document ).ready(function() {
-  console.log( "ready!" );
-  var time = 0,
-  elapsed = '0.0';
 
-//   var timer = function(){
-//     window.setInterval(function(){
-//       time += 1000;
-//       elapsed = Math.floor(time / 1000) / 1;
-//       console.log(elapsed)
-//     }, 1000);
-//   }
+  var clearInput = function(who){ who.val('') }
 
-// timer();
-
-var word_to_array = function(textContainer){
-  return array = $(textContainer).text().split(' ');
-}
-console.log(word_to_array('#text-to-input')[0][2]);
+  var word_to_array = function(textContainer){ return array = $(textContainer).text().split(' ') }
 
 
+  var
+  first_string = word_to_array('#text-to-input')[0];
+  // console.log(word_to_array('#text-to-input')[0][2]);
 
-var Timer = function() {
-  this.time = 0;
-  this.start = function(){
-   var that = this;
-   window.setInterval(function(){
-    that.time += 1000;
-    elapsed = Math.floor(that.time / 1000) / 1;
-    console.log(elapsed)
-  }, 1000);
- }
- this.stop = function(){
-  clearInterval( this );
-  elapsed = Math.floor(this.time / 1000) / 1;
-  return elapsed;
-}
-}
+  var Timer = function() {
+    this.time = 0;
+    this.start = function(){
+      var that = this;
+      window.setInterval(function(){
+        that.time += 1000;
+        elapsed = Math.floor(that.time / 1000) / 1;
+        console.log(elapsed)
+      }, 1000);
+    }
+    this.stop = function(){
+      clearInterval( this );
+      elapsed = Math.floor(this.time / 1000) / 1;
+      return elapsed;
+    }
+  }
+  var timer = new Timer();
+  $( "body" ).on( "click", ".start-time", function() {
+    timer.start();
+  });
+  $( "body" ).on( "click", ".stop-time", function() {
+    console.log( "current time at click: " + timer.stop() );
+  });
+  $("#GameInput").keyup(function(e){
 
-var timer = new Timer();
+    var val = $( this ).val();
+    if (val === first_string) {
+
+      console.log("Correct!");
+
+      $(this).keyup(function(e){
+       console.log("event2: " + e.which);
+       if(e.which == 32 ) {
+         clearInput($( this ));
+       }
+     })
+
+    };
 
 
-$( "body" ).on( "click", ".start-time", function() {
- timer.start();
-});
 
-$( "body" ).on( "click", ".stop-time", function() {
- console.log( "current time at click: " + timer.stop() );
-});
+    console.log(val);
+  });
 });
