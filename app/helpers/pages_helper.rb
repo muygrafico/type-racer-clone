a06708a721f6afd84f161ed43548d98c1e7f3f39
German Hernandez <muygrafico@gmail.com>
2015-04-26 17:21:15 -0500

    Fix current user

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -14,4 +14,20 @@ module PagesHelper
       accuracy = @user.races.average(:accuracy).round(2)
     end
   end
+
+  def current_average_wpm
+    if current_user.races.count == 0
+      average = 0
+    else
+      average = current_user.races.average(:wpm).round
+    end
+  end
+
+  def current_average_accuracy
+    if current_user.races.count == 0
+      accuracy = 0
+    else
+      accuracy = current_user.races.average(:accuracy).round(2)
+    end
+  end
 end
