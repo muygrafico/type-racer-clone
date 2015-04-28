9f2e1a72b339eb824908ef3c25a5f7b474ca2760
German Hernandez <muygrafico@gmail.com>
2015-04-27 22:37:25 -0500

    js perfomarce | less calls on validation

--- app/assets/javascripts/races-original.js
+++ app/assets/javascripts/races-original.js
@@ -1,1 +1,121 @@
-$(document).ready(function(){var e=function(e){e.val("")},t=function(e,t){this.keystrokeCounter=0,this.points=0,this.time=0,this.user_id=document.getElementsByClassName("userData")[0].getAttribute("data-user"),this.viewableNextWords=12,this.strings=this.word_to_array(t),this.next_words=e,this.viewHelper(e)};t.prototype={checkWord:function(e,t){return t=t.replace(/\s/g,""),e==t?!0:!1},word_to_array:function(e){return array=document.getElementById(e).innerHTML.split(" ")},sendGameData:function(e){$.ajax({url:"/races",type:"POST",data:e})},limitView:function(){this.stringsLength=this.strings.length,this.stringsLength-this.points<this.viewableNextWords&&(this.viewableNextWords=this.stringsLength-this.points,this.viewableNextWords<1&&(document.getElementsByClassName("timer").innerHTML="YOU FINISHED",$("#gameInput").prop("disabled",!0),this.elapsed=this.stop(),this.accuracy=s($("#gameText").text().length,this.keystrokeCounter),wpm=Math.round(this.stringsLength/(this.elapsed/60)),$("#sec").html("WPM: "+wpm),this.item={race:{wpm:wpm,accuracy:this.accuracy,finished_time:this.elapsed,user_id:this.user_id}},this.sendGameData(this.item),$(".new-game").removeClass("hide")))},viewHelper:function(){var e="";this.limitView();for(var t=0;t<this.viewableNextWords;t++)e+=0==t?"<strong>"+this.strings[this.points+t]+"</strong> ":this.strings[this.points+t]+" ";$(this.next_words).html(e)},start:function(){clearInterval(this.timer);var e=this;e.time=0,this.timer=setInterval(function(){e.time+=1e3,e.elapsed=Math.floor(e.time/1e3)/1,$("#sec").html(e.elapsed+" <span class='small'>sec</span>")},1e3)},stop:function(){return clearInterval(this.timer),elapsed=Math.floor(this.time/1e3)/1},counterBack:function(){}};var s=function(e,t){var s=e-(t-e);return part2=s/e,(100*part2).toFixed(2)};$("#gameInput").keyup(function(t){16!=t.which&&8!=t.which&&userRace.keystrokeCounter++;var s=$(this).val();userRace.viewHelper(),32==t.which&&1!=userRace.viewableNextWords?(word_validator=userRace.checkWord(userRace.strings[userRace.points],s),1==word_validator&&(e($(this)),userRace.points++,userRace.viewHelper())):1==userRace.viewableNextWords&&(word_validator=userRace.checkWord(userRace.strings[userRace.points],s),1==word_validator&&(e($(this)),userRace.points++,userRace.viewHelper()))}),$("#gameInput").attr("placeholder","Type the text above"),$("#gameInput").prop("disabled",!0),$(".timer").startTimer({onComplete:function(e){e.addClass("is-complete"),$(".timer").html("START!"),$("#gameInput").prop("disabled",!1),$("#gameInput").focus(),userRace=new t(".next-word","gameText",$("#gameText").length),userRace.start()}})});
+$( document ).ready(function() {
+
+  var clearInput = function(who){ who.val('') }
+
+  var Race = function( next_words, gameText) {
+    this.keystrokeCounter = 0;
+    this.points = 0;
+    this.time = 0;
+    this.user_id = document.getElementsByClassName( 'userData' )[0].getAttribute("data-user"); ;
+    this.viewableNextWords = 12;
+    this.strings = this.word_to_array( gameText );
+    this.next_words = next_words;
+    this.viewHelper( next_words );
+  }
+
+  Race.prototype = {
+    checkWord : function(array_word, word){
+      word =  word.replace(/\s/g, '');
+      if ( array_word  == word ) { return true } else { return false }
+    },
+  word_to_array : function( textContainer ){
+    return array = document.getElementById( textContainer ).innerHTML.split(' ')
+  },
+  sendGameData : function(item){
+    $.ajax({
+      url: "/races",
+      type: "POST",
+      data: item
+  })
+  },
+  limitView: function(){
+    this.stringsLength = this.strings.length;
+    if ( this.stringsLength - this.points < this.viewableNextWords ) {
+      this.viewableNextWords = this.stringsLength - this.points ;
+      if ( this.viewableNextWords < 1 ) {
+
+          document.getElementsByClassName("timer").innerHTML = "YOU FINISHED";
+
+          $("#gameInput").prop("disabled", true );
+          this.elapsed = this.stop();
+          this.accuracy = accuracy($('#gameText').text().length, this.keystrokeCounter);
+          wpm = Math.round( ( this.stringsLength / (this.elapsed/60) ) )
+
+          $("#sec").html( "WPM: " + wpm );
+          this.item = { race: {"wpm" : wpm, "accuracy" : this.accuracy, "finished_time": this.elapsed, "user_id": this.user_id }};
+          this.sendGameData( this.item );
+          $('.new-game').removeClass('hide');
+        };
+      };
+    },
+    viewHelper : function(){
+      var viewHelperText = "";
+      this.limitView();
+      for ( var i = 0; i < this.viewableNextWords ; i++ ) {
+        if ( i == 0 ) {
+          viewHelperText +=  "<strong>" +this.strings[this.points + i] + "</strong> "
+        } else {
+          viewHelperText += this.strings[this.points + i] + " "
+        }
+      };
+      $( this.next_words ).html( viewHelperText );
+    },
+    start : function(){
+      clearInterval( this.timer );
+      var that = this;
+      that.time = 0;
+      this.timer =  setInterval(function(){
+        that.time += 1000;
+        that.elapsed = Math.floor(that.time / 1000) / 1;
+        $("#sec").html( that.elapsed + " <span class='small'>sec</span>" )}, 1000);
+    },
+    stop : function(){
+      clearInterval( this.timer );
+      elapsed = Math.floor(this.time / 1000) / 1;
+      return elapsed;
+    },
+    counterBack : function(count_time){
+
+    }
+  }
+
+  function strip(number) {
+   return (parseFloat(number.toPrecision(12)));
+ }
+
+ var accuracy = function(text_length, counter){
+  var part1 = text_length - (counter - text_length )
+  part2 = part1 / text_length;
+  return (part2 * 100).toFixed(2) ;
+}
+
+$("#gameInput").keyup(function(e){
+
+  if ( e.which != 16 && e.which != 8 ) {
+    userRace.keystrokeCounter ++;
+  };
+
+  var val = $( this ).val();
+
+  if( e.which == 32  && userRace.viewableNextWords != 1 || userRace.viewableNextWords == 1 ) {
+    word_validator = userRace.checkWord( userRace.strings[ userRace.points ] , val) ;
+    if ( word_validator ){
+      clearInput( $( this ) );
+      userRace.points ++;
+      userRace.viewHelper();
+    }
+  }
+});
+
+$("#gameInput").attr("placeholder", "Type the text above");
+$("#gameInput").prop("disabled", true);
+$('.timer').startTimer({ onComplete: function(element){
+  element.addClass('is-complete')
+  $(".timer").html( "START!" );
+  $("#gameInput").prop("disabled", false);
+  $("#gameInput").focus();
+  userRace = new Race( '.next-word', 'gameText', $('#gameText').length );
+  userRace.start();
+} });
+
+});
